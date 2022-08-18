afa205efbdeb9a7d331fad1a33341b6b4cc1af1f
Hyun Yi <hy00un__@naver.com>
2022-08-18 19:44:32 +0900

    Update windowsPhotos.py

--- scripts/artifacts/windowsPhotos.py
+++ scripts/artifacts/windowsPhotos.py
@@ -18,11 +18,20 @@ def get_windowsPhotos(files_found, report_folder, seeker, wrap_text):
             item.Item_FileSize AS Item_FileSize,
             item.Item_Width AS Item_Width,
             item.Item_Height AS Item_Height,
+            item.Item_Latitude AS Item_Latitude,
+            item.Item_Longitude AS Item_Longitude,
+            ApplicationName.ApplicationName_Text,
+            CameraManufacturer.CameraManufacturer_Text,
+            CameraModel.CameraModel_Text,
             datetime((item.Item_DateTaken - 116444736000000000) / 10000000, 'unixepoch', 'localtime') AS Item_DateTaken,
             datetime((item.Item_DateCreated - 116444736000000000) / 10000000, 'unixepoch', 'localtime') AS Item_DateCreated,
             datetime((item.Item_DateModified - 116444736000000000) / 10000000, 'unixepoch', 'localtime') AS Item_DateModified,
             datetime((item.Item_DateIngested - 116444736000000000) / 10000000, 'unixepoch', 'localtime') AS Item_DateIngested
-            FROM item ORDER BY Item_DateCreated ASC''')
+            FROM item 
+            LEFT JOIN ApplicationName ON item.Item_ApplicationNameId = ApplicationName.ApplicationName_Id
+			LEFT JOIN CameraManufacturer ON item.Item_CameraManufacturerId = CameraManufacturer.CameraManufacturer_Id
+			LEFT JOIN CameraModel ON item.Item_CameraModelId = CameraModel.CameraModel_Id
+            ORDER BY Item_DateCreated ASC''')
 
             all_rows = cursor.fetchall()
             usageentries = len(all_rows)
@@ -36,9 +45,9 @@ def get_windowsPhotos(files_found, report_folder, seeker, wrap_text):
 
             data_list = []
 
-            data_headers = ('Filename', 'Filesize', 'Width', 'Height', 'DateTaken', 'DateCreated', 'DateModified', 'DateIngested')
+            data_headers = ('Filename', 'Filesize', 'Width', 'Height', 'Latitude', 'Longitude', 'ApplicationName', 'CameraManufacturer', 'CameraModel', 'DateTaken', 'DateCreated', 'DateModified', 'DateIngested')
             for rows in all_rows:
-                data_list.append((rows[0], rows[1], rows[2], rows[3], rows[4], rows[5], rows[6], rows[7]))
+                data_list.append((rows[0], rows[1], rows[2], rows[3], rows[4], rows[5], rows[6], rows[7], rows[8], rows[9], rows[10], rows[11], rows[12]))
 
             report.write_artifact_data_table(data_headers, data_list, file_found)
             report.end_artifact_report()
